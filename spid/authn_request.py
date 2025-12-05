a5740fdeaf08ea8a7b986c53c5fd3e01841e36e2
Gabriele <dotigabriele@gmail.com>
2025-12-05 18:56:04 +0100

    wait forward the mail answer

--- spid/authn_request.py
+++ spid/authn_request.py
@@ -62,19 +62,48 @@ def sign_xml(xml_str: str, reference_id: str) -> str:
     # Creation signer
     signer = XMLSigner(
         method=methods.enveloped,
-        signature_algorithm="rsa-"+settings.MD_ALG,
+        signature_algorithm="rsa-" + settings.MD_ALG,
         digest_algorithm=settings.MD_ALG,
         c14n_algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"
     )
     
-    # Find the node to sign by ID
+    # Find the node to sign by ID (AuthnRequest)
     node_to_sign = xml_doc.xpath(f"//*[@ID='{reference_id}']")[0]
     
-    # Sign
-    signed_root = signer.sign(node_to_sign, key=key_data, cert=cert_data, reference_uri=f"#{reference_id}")
+    # Sign the node
+    signed_root = signer.sign(
+        node_to_sign,
+        key=key_data,
+        cert=cert_data,
+        reference_uri=f"#{reference_id}"
+    )
 
-    # Return signed XML
-    return etree.tostring(signed_root, pretty_print=True, xml_declaration=True, encoding="UTF-8").decode("utf-8")
+    # ------------------------------
+    # Move Signature immediately after Issuer
+    # ------------------------------
+    signature = signed_root.find(".//{http://www.w3.org/2000/09/xmldsig#}Signature")
+    issuer = signed_root.find("{urn:oasis:names:tc:SAML:2.0:assertion}Issuer")
+
+    if signature is not None and issuer is not None:
+        # remove from current position
+        signed_root.remove(signature)
+        # insert right after Issuer
+        issuer_index = list(signed_root).index(issuer)
+        signed_root.insert(issuer_index + 1, signature)
+
+    # Serialize XML
+    xml_bytes = etree.tostring(
+        signed_root,
+        pretty_print=True,      # IMPORTANTISSIMO: non modificarlo
+        xml_declaration=False,
+        encoding="UTF-8"
+    )
+
+    with open("signed_authn_request.xml", "wb") as f:
+        f.write(xml_bytes)
+
+    # Return signed XML as string
+    return etree.tostring(signed_root, pretty_print=False, xml_declaration=False, encoding="UTF-8").decode("utf-8")
 
 def encode_authn_request(xml: str) -> str:
     deflated = zlib.compress(xml.encode())[2:-4]  # DEFLATE without header
@@ -106,26 +135,32 @@ def generate_authn_request_xml(sp_entity_id: str, name_qualifier: str, acs_url: 
     # Namespace
     NSMAP = {
         'samlp': "urn:oasis:names:tc:SAML:2.0:protocol",
-        'saml': "urn:oasis:names:tc:SAML:2.0:assertion",
-        'ds': "http://www.w3.org/2000/09/xmldsig#"
+        'saml': "urn:oasis:names:tc:SAML:2.0:assertion"#,
+        # 'ds': "http://www.w3.org/2000/09/xmldsig#"
     }
+
+    format_value = "urn:oasis:names:tc:SAML:2.0:nameid-format:entity"
     
     # Root AuthnRequest - POST Binding
     authn_request = etree.Element("{urn:oasis:names:tc:SAML:2.0:protocol}AuthnRequest",
-                                  nsmap=NSMAP,
-                                  AttributeConsumingServiceIndex="0",
-                                  AssertionConsumerServiceURL=acs_url,
-                                  ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",     # Binding per la risposta
-                                  Destination=idp_sso_url,
-                                  ForceAuthn="true",
-                                  ID=request_id,
-                                  IssueInstant=issue_instant,
-                                  Version="2.0")
+                                nsmap=NSMAP,
+                                ID=request_id,
+                                Version="2.0",
+                                IssueInstant=issue_instant,
+                                Destination=idp_sso_url,
+                                # ForceAuthn="true",
+                                AssertionConsumerServiceURL=acs_url,
+                                ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
+                                AttributeConsumingServiceIndex="0"
+                                )
     
     # Issuer
     issuer = etree.SubElement(authn_request, "{urn:oasis:names:tc:SAML:2.0:assertion}Issuer",
-                              NameQualifier=name_qualifier)
+                              NameQualifier=name_qualifier,
+                              Format=format_value)
     issuer.text = sp_entity_id
+
+    # QUA CI VA LA FIRMA XML
     
     # NameIDPolicy
     nameid_policy = etree.SubElement(authn_request, "{urn:oasis:names:tc:SAML:2.0:protocol}NameIDPolicy",
