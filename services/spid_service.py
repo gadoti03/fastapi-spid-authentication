8a20e83e2caaef2f6150e6ad4960559c79100974
Gabriele <dotigabriele@gmail.com>
2026-03-11 18:42:21 +0100

    added logout business logic

--- services/spid_service.py
+++ services/spid_service.py
@@ -3,21 +3,27 @@ from settings import settings
 from fastapi import HTTPException
 from fastapi.responses import FileResponse
 
-from spid.exceptions import MetadataNotFoundError
+from spid.exceptions import MetadataNotFoundError, SessionError
 
 import os
 
 from sqlalchemy.orm import Session
 from database.models import Session as DBSess
 
-from spid.authn_request import generate_authn_request, get_idp_url, render_saml_form
+from spid.authn_request import generate_authn_request, render_saml_form
 
+from spid.slo_handler import generate_logout_request
 from spid.utils import get_key_path, get_cert_path, sign_xml, encode_b64, get_field_in_xml, parse_query, verify_saml_status
+
+from spid.utils import get_idp_url
 
 from crud.saml_request import create_saml_request, use_saml_request
 
 from spid.acs_handler import verify_saml_signature as verify_saml_signature_acs, verify_saml_status, extract_spid_attributes
-from crud.session import get_or_create_session_by_session_id, update_session_with_spid_info, set_idp_id_by_session_id
+
+from spid.slo_handler import verify_saml_signature as verify_saml_signature_slo
+
+from crud.session import get_idp_id_by_id, get_or_create_session_by_session_id, invalidate_session_by_id, update_session_with_spid_info, set_idp_id_by_session_id, get_session_by_session_id
 from crud.user import create_user, get_user_by_cf, create_or_get_user
 
 from spid.exceptions import SpidConfigError, SpidSignatureError, SpidValidationError, SpidBusinessRuleError, SpidInternalError
@@ -30,10 +36,12 @@ def get_spid_metadata() -> FileResponse:
     else:
         raise MetadataNotFoundError("Metadata file not found")
 
-def initiate_authn_request(idp: str, db: Session, db_session: DBSess, relay_state: str):
+def initiate_authn_request(db: Session, idp: str, db_session: DBSess, relay_state: str):
 
     # get idp url
-    idp_url = get_idp_url(idp)
+    idp_url = get_idp_url(idp, "single_sign_on_service", "HTTP-POST")
+
+    # print("IDP URL:", idp_url + " for idp: " + idp)
 
     db_session = set_idp_id_by_session_id(db, db_session.session_id, idp) # aggiorno la sessione con l'idp scelto dall'utente
     if not db_session:
@@ -44,8 +52,6 @@ def initiate_authn_request(idp: str, db: Session, db_session: DBSess, relay_stat
     
     # save the SAML request in the database
     create_saml_request(db, request_id, db_session.id, "AuthnRequest")
-
-    # print("Request ID /login:", request_id)
     
     # sign the AuthnRequest XML 
     xml = sign_xml(xml_str = xml, key_path = get_key_path(), cert_path = get_cert_path(), after_tag="Issuer")
@@ -56,7 +62,7 @@ def initiate_authn_request(idp: str, db: Session, db_session: DBSess, relay_stat
     # render HTML con form auto-submit
     return render_saml_form(idp_url, saml_request, relay_state)
 
-def handle_authn_response(decoded_xml: bytes, db: Session, relay_state: str):
+def handle_authn_response(decoded_xml: bytes, db: Session):
 
     # verify signature
     if not verify_saml_signature_acs(decoded_xml):
@@ -107,3 +113,61 @@ def handle_authn_response(decoded_xml: bytes, db: Session, relay_state: str):
         raise SpidInternalError("Session not found or expired for session_id: " + saml_request.session_id)
     
     return session.session_id
+
+def initiate_logout_request( db: Session, db_session: DBSess, relay_state: str):
+
+    # verify session
+    if not db_session or not db_session.is_active:
+        raise SessionError("Session not found or expired for spid-logout for session_id: " + db_session.session_id)
+    
+    # invalidate session in the database (set is_active to False)
+    invalidate_session_by_id(db, db_session.id)
+    
+    # get idp_url
+    idp_id = get_idp_id_by_id(db, db_session.id)
+    url_slo = get_idp_url(idp_id, "single_logout_service", "HTTP-Redirect")
+
+    # generate LogoutRequest
+    xml, request_id = generate_logout_request(db_session.session_id, idp_id, url_slo)
+    
+    # save the SAML request in the database
+    create_saml_request(db, request_id, db_session.id, "LogoutRequest")
+
+    # sign the LogoutRequest XML 
+    xml = sign_xml(xml_str = xml, key_path = get_key_path(), cert_path = get_cert_path(), after_tag="SessionIndex")
+    
+    # base64 encode the signed LogoutRequest XML
+    saml_request = encode_b64(xml)
+    
+    # render HTML con form auto-submit
+    return render_saml_form(url_slo, saml_request, relay_state)
+
+def handle_logout_response(db: Session, raw_query: str):
+
+    # parse query parameters
+    slo_data = parse_query(raw_query)
+
+    decoded_xml = slo_data["decoded_xml"]
+    relay_state = slo_data["RelayState"]
+    sig_alg = slo_data["SigAlg"]
+    signature = slo_data["Signature"]
+    query_to_verify = slo_data["query_to_verify"]
+
+    if not verify_saml_signature_slo(query_to_verify, signature, sig_alg, decoded_xml):
+        raise SpidSignatureError("Invalid SAML signature in LogoutResponse")
+    
+    # verify status
+    if not verify_saml_status(decoded_xml):
+        raise SpidValidationError("Invalid SAML status in LogoutResponse")
+    
+    # get RequestID
+    request_id = get_field_in_xml(decoded_xml, "InResponseTo")
+    if not request_id:
+        raise SpidValidationError("InResponseTo not found in SAML LogoutResponse")
+    
+    # verify that the SAML response corresponds to a valid SAML request in the database
+    saml_request = use_saml_request(db, request_id)
+    if not saml_request:
+        raise SpidBusinessRuleError("No matching SAML request found for InResponseTo: " + request_id)
+    
+    return relay_state
